0d2c39b15cfe61fa3440d2c698c0d994dbdc6700
virkkno <helmi.virkki@gmail.com>
2023-08-30 15:47:35 +0300

    hw03 hopefully

---
+++ BooleanHW03/src/Main1.java
@@ -0,0 +1,13 @@
+public class Main1 {
+    public static void main(String[] args) {
+       boolean flag= false;
+        int a = 14, b =733;
+       if(a%10 == b%10){
+           flag= true;
+           System.out.println(flag);
+       }
+       else{
+           System.out.println(flag);
+       }
+    }
+}
