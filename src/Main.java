0ff195283ae6811531ec5da8ebe8b49bdeaa9dd2
virkkno <helmi.virkki@gmail.com>
2023-08-30 15:22:01 +0300

    jj

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
-        boolean found = false;
-        int r, a = 7, b = 22;
-        while (!found) {
-            r = a % b;
-            if (r == 0) {
-                found = true;
-                System.out.println("solution is " + b);
-            }
-            else{
-                a=b;
-                b=r;
-            }
-        }
+        System.out.println("Hello world");
     }
 }
